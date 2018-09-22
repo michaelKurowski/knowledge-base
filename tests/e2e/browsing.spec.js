38d0a55ccdf0dd13236f1e7571f1223ae905b645
michaelKurowski <Byamarro@gmail.com>
2018-09-23 00:28:01 +0200

    browsing e2e listing by category tests draft

---
+++ tests/e2e/browsing.spec.js
@@ -0,0 +1,35 @@
+const spawnSync = require('child_process').spawnSync
+
+
+let suite
+beforeEach(() => {
+    suite = {}
+})
+
+afterEach(() => {
+
+})
+
+describe('E2E: Browsing', () => {
+    describe('Listing by categories', () => {
+        it('throws error when no category was provided', () => {
+
+        })
+
+        it('throws errror when provided category does not exists', () => {
+
+        })
+
+        it('throws error when space was provided as category', () => {
+
+        })
+
+        it('shows results for correct category', () => {
+
+        })
+    })
+
+    describe('Querrying', () => {
+
+    })
+})
